35953ec6a2dd71d86c26b3d3b717caa2c2a5c120
Maksym7415 <popovmaksim7415@gmail.com>
2026-07-22 15:05:14 +0200

    updated routing, added local and tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 import { AuthProvider } from "./modules/auth/AuthContext";
-import "./app/styles.css";
+import "./app/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
